7973b3fa3f6715233a27832decf9239d7e6a37c8
Artem Lymarenko <artemchiklym@gmail.com>
2023-12-08 22:51:17 +0200

    Added protection for users and admins

--- frontend/src/components/router/Router.tsx
+++ frontend/src/components/router/Router.tsx
@@ -4,20 +4,43 @@ import CategoryPage from '@/components/screens/categories/CategoryPage'
 import NotFound from '../screens/not-found/NotFound'
 import ProductDetailsPage from '../screens/product-page/ProductDetailsPage'
 import AuthPage from '../screens/auth-page/AuthPage'
-//import AuthProvider from '@/providers/auth-provider/AuthProvider'
+import AuthProvider from '@/providers/auth-provider/AuthProvider'
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route
-					// <AuthProvider role={{ isUser: true }} children={<Home />}
-					element={<Home />}
+					element={<AuthProvider role={{}} children={<Home />} />}
 					path='/'
 				></Route>
-				<Route element={<CategoryPage />} path='/categories/:slug'></Route>
-				<Route element={<ProductDetailsPage />} path='/products/:id'></Route>
+				<Route
+					element={<AuthProvider role={{}} children={<CategoryPage />} />}
+					path='/categories/:slug'
+				></Route>
+				<Route
+					element={<AuthProvider role={{}} children={<ProductDetailsPage />} />}
+					path='/products/:id'
+				></Route>
 				<Route element={<AuthPage />} path='/auth'></Route>
+				<Route
+					element={
+						<AuthProvider
+							role={{ isUser: true }}
+							children={<div>User cabinet</div>}
+						/>
+					}
+					path='/my-cabinet'
+				></Route>
+				<Route
+					element={
+						<AuthProvider
+							role={{ isAdmin: true }}
+							children={<div>Admin panel</div>}
+						/>
+					}
+					path='/admin-panel'
+				></Route>
 				<Route path='*' element={<NotFound />}></Route>
 			</Routes>
 		</BrowserRouter>
