2ed40ce23903f15467c898d3d3bcadb1941aaa88
deykun04 <maslovsasapvp@gmail.com>
2023-12-11 15:34:19 +0200

    -  try to add statistics ( withou tests at the moment )

--- backend/src/statistics/statistics.module.ts
+++ backend/src/statistics/statistics.module.ts
@@ -3,9 +3,11 @@ import { StatisticsService } from './statistics.service';
 import { StatisticsController } from './statistics.controller';
 import { PrismaService } from 'src/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { ProductService } from 'src/product/product.service';
+import { OrderService } from 'src/order/order.service';
 
 @Module({
   controllers: [StatisticsController],
-  providers: [StatisticsService,PrismaService,UserService],
+  providers: [StatisticsService,ProductService,OrderService,PrismaService,UserService],
 })
 export class StatisticsModule {}
--- backend/src/statistics/statistics.service.ts
+++ backend/src/statistics/statistics.service.ts
@@ -1,33 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { ProductService } from 'src/product/product.service';
+import { OrderService } from 'src/order/order.service';
 import { UserService } from 'src/user/user.service';
+import { Decimal } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class StatisticsService {
   constructor(
     private prisma: PrismaService,
-    private userService : UserService){}
+    private productService: ProductService,
+    private orderService: OrderService,
+    private userService: UserService,
+  ) {}
 
-  async getMain(userID: number){
-    const user = await this.userService.getUserById(userID,{
-      orders:{
-        select:{
-          items: true,
-        }
-      }
-    })
-
-    
-    return[
-      {
-        name:'Total users',
-        value: user._count
-      },
-      {
-        name:'Orders',
-        value: user.orders.length
-      }
-    ]
-
+  async getTotalUsers(): Promise<number> {
+    const usersCount = await this.prisma.user.count();
+    return usersCount;
   }
+
+  async getTotalProducts(): Promise<number> {
+    const productsCount = await this.prisma.product.count();
+    return productsCount;
+  }
+
+  async getTotalOrders(): Promise<number> {
+    const ordersCount = await this.orderService.getAll().then((orders) => orders.length);
+    return ordersCount;
+  }
+
+  async getTotalIncome(): Promise<number> {
+    const products = await this.productService.getAll();
+    const totalIncome = products.reduce((sum, product) => {
+      return sum.plus(new Decimal(product.price));
+    }, new Decimal(0));
+    return totalIncome.toNumber(); // Convert Decimal back to number
+  }
+  
 }
