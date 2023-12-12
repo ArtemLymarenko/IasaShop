b8d7a051915f28edc6a57e259f35795e2dcf491c
deykun04 <maslovsasapvp@gmail.com>
2023-12-12 18:12:45 +0200

    -added navigation in user account -added styles for cabinet

--- frontend/src/components/screens/user-page/UserPage.tsx
+++ frontend/src/components/screens/user-page/UserPage.tsx
@@ -1,103 +1,147 @@
+import React, { FC, useState, useEffect } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
+import Layout from '@/components/layout/Layout'
+import styles from './UserPage.module.scss'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import Field from '@/components/ui/input/Field'
+import { IUser } from '@/types/user.interface'
+import userService from '@/components/services/user/user.service'
+import { useGetProfile } from '@/hooks/useGetProfile'
+import { useActions } from '@/hooks/useActions'
+import Heading from '@/components/ui/heading/Heading'
+import Button from '@/components/ui/button/Button'
 
-import { FC, useState, useEffect } from 'react';
-import { useQueryClient } from '@tanstack/react-query';
-import Layout from '@/components/layout/Layout';
-import styles from './UserPage.module.scss';
-import { useForm, SubmitHandler } from 'react-hook-form';
-import Button from '@/components/ui/button/Button';
-import Field from '@/components/ui/input/Field';
-import { IUser } from '@/types/user.interface';
-import userService from '@/components/services/user/user.service';
-import { useGetProfile } from '@/hooks/useGetProfile';
-import { useActions } from '@/hooks/useActions';
-import Heading from '@/components/ui/heading/Heading';
+const LeftSection: FC<{ onButtonClick: (button: string) => void }> = ({
+	onButtonClick
+}) => {
+	return (
+		<div className={styles.leftSection}>
+			<div className={styles.textBlock}>
+				<div
+					className={styles.leftSectionText}
+					onClick={() => onButtonClick('account')}
+				>
+					Personal Account
+				</div>
+				<div
+					className={styles.leftSectionText}
+					onClick={() => onButtonClick('orders')}
+				>
+					My Orders
+				</div>
+			</div>
+		</div>
+	)
+}
 
 const UserPage: FC<{ pageTitle: string }> = ({ pageTitle }) => {
-  const queryClient = useQueryClient();
-  const userProfile = useGetProfile();
-  const { logout } = useActions()
-  const [errorMessage, setErrorMessage] = useState('');
-  const {
-    register: formRegister,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    setValue, 
-  } = useForm<IUser>({
-    mode: 'onChange',
-    defaultValues: userProfile,
-  });
+	const queryClient = useQueryClient()
+	const userProfile = useGetProfile()
+	const { logout } = useActions()
+	const [errorMessage, setErrorMessage] = useState('')
+	const [selectedButton, setSelectedButton] = useState<string>('account') // Default to 'account'
 
-  useEffect(() => {
-    setValue('firstName', userProfile?.firstName || '');
-    setValue('lastName', userProfile?.lastName || '');
-    setValue('phone', userProfile?.phone || '');
-    setValue('email', userProfile?.email || '');
-  }, [userProfile, setValue]);
+	const {
+		register: formRegister,
+		handleSubmit,
+		formState: { errors },
+		reset,
+		setValue
+	} = useForm<IUser>({
+		mode: 'onChange',
+		defaultValues: userProfile
+	})
 
-  const onSubmit: SubmitHandler<IUser> = async (data) => {
-    console.log('Submitting data:', data);
-    const user = await userService.updateProfile({
-      email: data.email,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      phone: data.phone,
-    });
+	useEffect(() => {
+		setValue('firstName', userProfile?.firstName || '')
+		setValue('lastName', userProfile?.lastName || '')
+		setValue('phone', userProfile?.phone || '')
+		setValue('email', userProfile?.email || '')
+	}, [userProfile, setValue])
 
-    if (!user) {
-      setErrorMessage('Something went wrong. Try again!');
-      return;
-    }
+	const onSubmit: SubmitHandler<IUser> = async data => {
+		console.log('Submitting data:', data)
+		const user = await userService.updateProfile({
+			email: data.email,
+			firstName: data.firstName,
+			lastName: data.lastName,
+			phone: data.phone
+		})
 
+		if (!user) {
+			setErrorMessage('Something went wrong. Try again!')
+			return
+		}
 
-    queryClient.invalidateQueries('profile data');
+		queryClient.invalidateQueries('profile data')
 
-    reset();
-  };
+		reset()
+	}
 
-  return (
-    <Layout pageTitle={pageTitle}>
-      <div className='userInfo'>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-		<Field
-			  {...formRegister('firstName', {
-				required: 'First name is required',
-			  })}
-			  placeholder='First Name'
-			  error={errors.firstName?.message}
-			/>
-  
-			<Field
-			  {...formRegister('lastName', {
-				required: 'Last name is required',
-			  })}
-			  placeholder='Last Name'
-			  error={errors.lastName?.message}
-			/>
-  
-			<Field
-			  {...formRegister('phone', {
-				required: 'Phone number is required',
-			  })}
-			  placeholder='Phone'
-			  error={errors.phone?.message}
-			/>
-  
-			<Field
-			  {...formRegister('email', {
-				required: 'Email is required',
-			  })}
-			  placeholder='Email'
-			  error={errors.email?.message}
-			/>
+	const renderRightSection = () => {
+		switch (selectedButton.toLowerCase()) {
+			case 'account':
+				return (
+					<>
+						<Heading>My Account</Heading>
+						<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+							<Field
+								{...formRegister('firstName', {
+									required: 'First name is required'
+								})}
+								placeholder='First Name'
+								error={errors.firstName?.message}
+							/>
 
-          <Button type='submit'>Save</Button>
-		  <Button onClick={logout}>Logout</Button>
-        </form>
-      </div>
-    </Layout>
-  );
-};
+							<Field
+								{...formRegister('lastName', {
+									required: 'Last name is required'
+								})}
+								placeholder='Last Name'
+								error={errors.lastName?.message}
+							/>
 
-export default UserPage;
+							<Field
+								{...formRegister('phone', {
+									required: 'Phone number is required'
+								})}
+								placeholder='Phone'
+								error={errors.phone?.message}
+							/>
 
+							<Field
+								{...formRegister('email', {
+									required: 'Email is required'
+								})}
+								placeholder='Email'
+								error={errors.email?.message}
+							/>
+              <Button type="submit" className={styles.submitButton}>Save</Button>
+						</form>
+					</>
+				)
+			case 'orders':
+				return (
+					<>
+						<Heading>My Orders</Heading>
+					</>
+				)
+			default:
+				return null
+		}
+	}
+
+	return (
+		<Layout pageTitle={pageTitle}>
+			<div className={styles.pageContainer}>
+				<LeftSection onButtonClick={button => setSelectedButton(button)} />
+
+				<div className={styles.rightSection}>
+					<div className={styles.rightContent}>{renderRightSection()}</div>
+				</div>
+			</div>
+		</Layout>
+	)
+}
+
+export default UserPage
