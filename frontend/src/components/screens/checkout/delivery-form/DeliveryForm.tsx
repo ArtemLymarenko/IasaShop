f9fd91fb98d9ac1ea574a80e25d5c920fccb028c
deykun04 <maslovsasapvp@gmail.com>
2023-12-16 14:08:59 +0200

    - added save info button and saving information about user into DTOs

--- frontend/src/components/screens/checkout/delivery-form/DeliveryForm.tsx
+++ frontend/src/components/screens/checkout/delivery-form/DeliveryForm.tsx
@@ -1,74 +1,107 @@
-import { FC, useState } from 'react'
-import Field from '@/components/ui/input/Field'
-import styles from './DeliveryForm.module.scss'
-import { IOrder } from '@/types/order.interface'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import Button from '@/components/ui/button/Button'
+import { Dispatch, FC, SetStateAction } from 'react';
+import Field from '@/components/ui/input/Field';
+import styles from './DeliveryForm.module.scss';
+import { IOrder } from '@/types/order.interface';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import Button from '@/components/ui/button/Button';
 
-const DeliveryForm: FC = () => {
-	const [errorMessage, setErrorMessage] = useState('')
-	const {
-		register: formRegister,
-		handleSubmit,
-		formState: { errors },
-		reset
-	} = useForm<IOrder>({
-		mode: 'onChange'
-	})
+interface DeliveryFormProps {
+	setDeliveryInfo: Dispatch<
+	  SetStateAction<{
+		shipCountry: string;
+		shipCity: string;
+		shipRegion: string;
+		shipPostalCode: string;
+		shipAdress: string;
+	  }>
+	>;
+  }
 
-	const onSubmit: SubmitHandler<IOrder> = data => {
-		reset()
-	}
+const DeliveryForm: FC<DeliveryFormProps> = ({ setDeliveryInfo }) => {
+  const {
+    register: formRegister,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<Partial<IOrder>>({
+    mode: 'onChange',
+  });
 
-	return (
-		<div className={styles.formContainer}>
-			<p>Fill in your personal informaiton</p>
-			<p className={styles.errorMessage}>{errorMessage}</p>
-			<form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-				<Field
-					{...formRegister('shipCity', {
-						required: 'City is required'
-					})}
-					placeholder='Ship city'
-					error={errors.shipCity?.message}
-				/>
-				<Field
-					{...formRegister('shipCountry', {
-						required: 'Coutry is required'
-					})}
-					placeholder='Ship country'
-					error={errors.shipCountry?.message}
-				/>
-				<Field
-					{...formRegister('shipRegion', {
-						required: 'Region is required'
-					})}
-					placeholder='Ship region'
-					error={errors.shipRegion?.message}
-				/>
-				<Field
-					{...formRegister('shipPostalCode', {
-						required: 'Postal code is required',
-						pattern: {
-							value: /^[0-9]+$/,
-							message: 'Postal code must contain only digits'
-						}
-					})}
-					placeholder='Postal Code'
-					error={errors.shipPostalCode?.message}
-				/>
-				<Field
-					{...formRegister('shipAdress', {
-						required: 'Address is required'
-					})}
-					placeholder='Your address'
-					error={errors.shipAdress?.message}
-				/>
+  const onSubmit: SubmitHandler<Partial<IOrder>> = (data) => {
+    // Ensure that properties are not undefined before setting the state
+    const sanitizedData: {
+      shipCountry: string;
+      shipCity: string;
+      shipRegion: string;
+      shipPostalCode: string;
+      shipAdress: string;
+    } = {
+      shipCountry: data.shipCountry || '', // Default to empty string if undefined
+      shipCity: data.shipCity || '',
+      shipRegion: data.shipRegion || '',
+      shipPostalCode: data.shipPostalCode || '',
+      shipAdress: data.shipAdress || '',
+    };
 
-				<Button type='submit'>Place order</Button>
-			</form>
-		</div>
-	)
-}
+    setDeliveryInfo(sanitizedData);
+    reset();
+  };
 
-export default DeliveryForm
+  return (
+    <div className={styles.formContainer}>
+      <p>Fill in your personal information</p>
+      <div className={styles.errorMessage}>
+        {errors.shipCity && <p>{errors.shipCity.message}</p>}
+        {errors.shipCountry && <p>{errors.shipCountry.message}</p>}
+        {errors.shipRegion && <p>{errors.shipRegion.message}</p>}
+        {errors.shipPostalCode && <p>{errors.shipPostalCode.message}</p>}
+        {errors.shipAdress && <p>{errors.shipAdress.message}</p>}
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <Field
+          {...formRegister('shipCity', {
+            required: 'City is required'
+          })}
+          placeholder='Ship city'
+          error={errors.shipCity?.message}
+        />
+        <Field
+          {...formRegister('shipCountry', {
+            required: 'Coutry is required'
+          })}
+          placeholder='Ship country'
+          error={errors.shipCountry?.message}
+        />
+        <Field
+          {...formRegister('shipRegion', {
+            required: 'Region is required'
+          })}
+          placeholder='Ship region'
+          error={errors.shipRegion?.message}
+        />
+        <Field
+          {...formRegister('shipPostalCode', {
+            required: 'Postal code is required',
+            pattern: {
+              value: /^[0-9]+$/,
+              message: 'Postal code must contain only digits'
+            }
+          })}
+          placeholder='Postal Code'
+          error={errors.shipPostalCode?.message}
+        />
+        <Field
+          {...formRegister('shipAdress', {
+            required: 'Address is required'
+          })}
+          placeholder='Your address'
+          error={errors.shipAdress?.message}
+        />
+
+        <Button type='submit'>Save</Button>
+      </form>
+    </div>
+  );
+};
+
+export default DeliveryForm;
